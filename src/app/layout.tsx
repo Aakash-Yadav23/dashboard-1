2f570b3837f88e2efdeead79a8a4510c3a093185
namansingh98 <naman.rathore@outlook.com>
2024-03-18 22:44:28 +0530

    some updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/lib/Theme-provider";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn(mont.className)}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(mont.className)}>
+      <ThemeProvider defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+        </body>
     </html>
   );
 }
